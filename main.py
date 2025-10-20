d97544d9a973d79c5670f48c1ba19d95ab6dedae
lemmonboys99 <lemmonboys99@gmail.com>
2025-10-19 20:20:40 -0700

    added apples

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import pygame,sys
+import pygame,sys,random
 from pygame.math import Vector2
 
 pygame.init()
@@ -9,27 +9,34 @@ DARKBLUE = (8,96,168)
 cell_size = 30 
 num_of_cells = 25
 
-print('hi')
-
-class Food:
+class Food():
     def __init__(self):
-        self.position = Vector2(5,6)
+        self.position = self.gen_random_pos()
+
     def draw(self):
         food_rect = pygame.Rect(self.position.x * cell_size, self.position.y * cell_size, cell_size, cell_size )#x,y,w,h of rect
-        pygame.draw.rect(screen, DARKBLUE, food_rect)
+        screen.blit(scaled_food_surface, food_rect)
 
-
+    def gen_random_pos(self):
+        X = random.randint(0, num_of_cells-1)
+        Y = random.randint(0, num_of_cells-1)
+        pos = Vector2(X,Y)
+        return pos
 screen = pygame.display.set_mode((cell_size*num_of_cells,cell_size*num_of_cells))
-
 pygame.display.set_caption('SnakeGame1')
-
 clock = pygame.time.Clock()
+
+food = Food()
+food_surface = pygame.image.load(".\\graphics\\food.png").convert_alpha()
+scaled_food_surface = pygame.transform.scale(food_surface, (cell_size,cell_size))
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+    #drawing
     screen.fill(BLUE)
+    food.draw()
     pygame.display.update()
     clock.tick(60)
